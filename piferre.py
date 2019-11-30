2a9db91371c098e5e7cb88537cd3275967b641a8
Carlos Allende <callende@iac.es>
2019-11-30 05:51:16 +0000

    added target ids used in ferre (fid) to the par files; limited the fmp info to the targets that are run through ferre

--- piferre.py
+++ piferre.py
@@ -775,7 +775,8 @@ def do(path,pixel,sdir='',truth=None,nthreads=1):
       savetxt(os.path.join(sdir,pixel,pixel)+suffix+'-'+bands[j]+'.wav',x1,fmt='%14.5e')
 
     savetxt(os.path.join(sdir,pixel,pixel)+suffix+'.wav',xx,fmt='%14.5e')
-    fibermap = fibermap [(mws_target == 1)]
+    wsection = (mws_target == 1)
+    fibermap = fibermap [wsection]
     hdu0 = fits.BinTableHDU.from_columns(fibermap)
     hdu0.writeto(os.path.join(sdir,pixel,pixel)+suffix+'.fmp.fits')
     print (yy.shape)
