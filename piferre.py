41d6e40297c5d634499cc5a13e2643d40bb3fb8a
Carlos Allende <callende@iac.es>
2022-12-16 15:08:55 +0000

    fixed bug in packfits

--- piferre.py
+++ piferre.py
@@ -1961,9 +1961,9 @@ def packfits(input="*.fits",output="output.fits",update_srcfile=False):
           except AttributeError:
             print('Warning: the file ',entry,' does not have the attribute columns ',' in extension ',i,' -- ',hdu.header['EXTNAME']) 
           if update_srcfile and colname == 'SRCFILE':
-            if entry == f[1]: hdu.data[colname][:nrows1] = f[0].split(os.path.sep)[-1]
+            if entry == f[1]: hdu.data[colname][:nrows1] = f[0].split(os.path.sep)[-1].split('-')[1:]
             hdu.data[colname][nrows1:] = entry.split(os.path.sep)[-1]
-            print(len(hdu.data[colname][nrows1]),len(entry.split(os.path.sep)[-1]))
+            print(len(hdu.data[colname][nrows1]),len(entry.split(os.path.sep)[-1])).split('-')[1:]
 
               
 
